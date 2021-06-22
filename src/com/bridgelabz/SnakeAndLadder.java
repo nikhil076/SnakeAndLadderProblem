b8c55abb54bd77170c84d8fb24ecea80b3abb2af
nikhil076 <nikhil.arvind7@gmail.com>
2021-06-22 13:34:45 +0530

    getting random

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -5,5 +5,9 @@ public class SnakeAndLadder {
 	public static void main(String args[]) {
 		System.out.println("Welcome to snake and ladder ");
 		int PLAYER1_STARTING_POSITION = 0;
+		
+		double DiceRoll = Math.floor(Math.random() * 10 ) % 6;
+		DiceRoll+=1;
+		System.out.println(DiceRoll);
 	}
 }
