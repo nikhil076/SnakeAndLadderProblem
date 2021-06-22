d810e746630dbac24d9471437ad783f9fca327e9
nikhil076 <nikhil.arvind7@gmail.com>
2021-06-22 23:08:18 +0530

    added player 2 and checked who has won

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -9,33 +9,66 @@ public class SnakeAndLadder {
 		
 			System.out.println("Welcome to snake and ladder ");
 			int PLAYER1_STARTING_POSITION = 0;
-			int COUNT_OF_DICE_ROLL = 0;
+			int PLAYER2_STARTING_POSITION = 0;
+			int COUNT_OF_PLAYER1_DICE_ROLL = 0;
+			int COUNT_OF_PLAYER2_DICE_ROLL = 0;
 			
-			while(PLAYER1_STARTING_POSITION<100)
+			while(PLAYER1_STARTING_POSITION<100 && PLAYER2_STARTING_POSITION<100)
 			{
-				double DICE_ROLL = Math.floor(Math.random() * 10 ) % 6;
-				DICE_ROLL+=1;
-				System.out.println(DICE_ROLL);
+				// for player one
+				double PLAYER1_DICE_ROLL = Math.floor(Math.random() * 10 ) % 6;
+				PLAYER1_DICE_ROLL+=1;
+				System.out.println(PLAYER1_DICE_ROLL);
 				
-				double OPTIONS = Math.floor(Math.random() * 10 ) % 3;
-				System.out.println(OPTIONS);
+				double PLAYER1_OPTIONS = Math.floor(Math.random() * 10 ) % 3;
+				System.out.println(PLAYER1_OPTIONS);
 			
-				switch ((int)OPTIONS) {
+				switch ((int)PLAYER1_OPTIONS) {
 				case LADDER : 
-					PLAYER1_STARTING_POSITION+=DICE_ROLL;
+					PLAYER1_STARTING_POSITION+=PLAYER1_DICE_ROLL;
 					if((int)PLAYER1_STARTING_POSITION > 100) {
-						PLAYER1_STARTING_POSITION=(int) (PLAYER1_STARTING_POSITION-DICE_ROLL);
+						PLAYER1_STARTING_POSITION=(int) (PLAYER1_STARTING_POSITION-PLAYER1_DICE_ROLL);
 					}
-					System.out.println("Ladder has occured and position now is :"+PLAYER1_STARTING_POSITION);
-					COUNT_OF_DICE_ROLL++;
+					System.out.println("Player 1 has won and position now is :"+PLAYER1_STARTING_POSITION);
+					COUNT_OF_PLAYER1_DICE_ROLL++;
 					break;
 					
 				case SNAKE :
-					PLAYER1_STARTING_POSITION = (int) (PLAYER1_STARTING_POSITION-DICE_ROLL) ;
+					PLAYER1_STARTING_POSITION = (int) (PLAYER1_STARTING_POSITION-PLAYER1_DICE_ROLL) ;
 					if(PLAYER1_STARTING_POSITION < 0) {
 						PLAYER1_STARTING_POSITION=0;
 					}
-					System.out.println("Snake has occured aand new position is "+PLAYER1_STARTING_POSITION);
+					System.out.println("Snake has occured and new position is "+PLAYER1_STARTING_POSITION);
+					break;
+				
+				default:
+					System.out.println("No play has occured , no chanage in position");
+					break;
+				}
+				// for player two
+				double PLAYER2_DICE_ROLL = Math.floor(Math.random() * 10 ) % 6;
+				PLAYER2_DICE_ROLL+=1;
+				System.out.println(PLAYER2_DICE_ROLL);
+				
+				double PLAYER2_OPTIONS = Math.floor(Math.random() * 10 ) % 3;
+				System.out.println(PLAYER2_OPTIONS);
+			
+				switch ((int)PLAYER2_OPTIONS) {
+				case LADDER : 
+					PLAYER2_STARTING_POSITION+=PLAYER2_DICE_ROLL;
+					if((int)PLAYER2_STARTING_POSITION > 100) {
+						PLAYER2_STARTING_POSITION=(int) (PLAYER2_STARTING_POSITION-PLAYER2_DICE_ROLL);
+					}
+					System.out.println("player 2 has won and position now is :"+PLAYER2_STARTING_POSITION);
+					COUNT_OF_PLAYER2_DICE_ROLL++;
+					break;
+					
+				case SNAKE :
+					PLAYER2_STARTING_POSITION = (int) (PLAYER2_STARTING_POSITION-PLAYER2_DICE_ROLL) ;
+					if(PLAYER2_STARTING_POSITION < 0) {
+						PLAYER2_STARTING_POSITION=0;
+					}
+					System.out.println("Snake has occured and new position is "+PLAYER2_STARTING_POSITION);
 					break;
 				
 				default:
@@ -43,6 +76,7 @@ public class SnakeAndLadder {
 					break;
 				}
 			}
-		System.out.println("Number of time the dice is rolled to win is "+COUNT_OF_DICE_ROLL);
+		System.out.println("Number of time player 1 dice is rolled to win is "+COUNT_OF_PLAYER1_DICE_ROLL);
+		System.out.println("Number of time player 2 dice is rolled to win is "+COUNT_OF_PLAYER2_DICE_ROLL);
 	}
 }
