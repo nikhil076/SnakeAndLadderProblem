8a89ef4f5ac8164a31810c87feb18131e77b6901
nikhil076 <nikhil.arvind7@gmail.com>
2021-06-22 14:32:56 +0530

    postion resets to 0 if its below 0

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -9,23 +9,25 @@ public class SnakeAndLadder {
 		
 			System.out.println("Welcome to snake and ladder ");
 			int PLAYER1_STARTING_POSITION = 0;
-			double DICE_ROLL = Math.floor(Math.random() * 10 ) % 6;
-			DICE_ROLL+=1;
-			System.out.println(DICE_ROLL);
+			
+			while(PLAYER1_STARTING_POSITION<10)
+			{
+				double DICE_ROLL = Math.floor(Math.random() * 10 ) % 6;
+				DICE_ROLL+=1;
+				System.out.println(DICE_ROLL);
 				
-			double OPTIONS = Math.floor(Math.random() * 10 ) % 3;
-			System.out.println(OPTIONS);
+				double OPTIONS = Math.floor(Math.random() * 10 ) % 3;
+				System.out.println(OPTIONS);
 			
-			switch ((int)OPTIONS) {
-			
+				switch ((int)OPTIONS) {
 				case LADDER : 
 					PLAYER1_STARTING_POSITION+=DICE_ROLL;
-					if((int)PLAYER1_STARTING_POSITION > 100) {
+					if((int)PLAYER1_STARTING_POSITION > 10) {
 						PLAYER1_STARTING_POSITION=(int) (PLAYER1_STARTING_POSITION-DICE_ROLL);
 					}
 					System.out.println("Ladder has occured and position now is :"+PLAYER1_STARTING_POSITION);
 					break;
-						
+					
 				case SNAKE :
 					PLAYER1_STARTING_POSITION = (int) (PLAYER1_STARTING_POSITION-DICE_ROLL) ;
 					if(PLAYER1_STARTING_POSITION < 0) {
@@ -33,10 +35,11 @@ public class SnakeAndLadder {
 					}
 					System.out.println("Snake has occured aand new position is "+PLAYER1_STARTING_POSITION);
 					break;
-					
+				
 				default:
 					System.out.println("No play has occured , no chanage in position");
 					break;
+				}
 			}
 	}
 }
